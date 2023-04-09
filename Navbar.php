9b780dac9aed26333ec956086815668e0d72f54a
VirgrowAtlas <112686395+VirgrowAtlas@users.noreply.github.com>
2023-04-09 07:39:40 +0700

    Add files via upload

---
+++ Navbar.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="style.css">
+</head>
+<body>
+<navbar class="navbar-container">
+        <div class="logo-container">
+            <a href="#">QR Store</a>
+        </div>
+
+        <div class="bars">
+            <div class="bar"></div>
+            <div class="bar"></div>
+            <div class="bar"></div>
+        </div>
+
+        <ul class="nav-items">
+            <li class="nav-link"><a href="Home.php">Home</a></li>
+            <li class="nav-link"><a href="#">Service</a></li>
+            <li class="nav-link"><a href="#">Projects</a></li>
+            <li class="nav-link"><a href="#">About</a></li>
+            <div class="login-register">
+                <a href="login.php" class="button">Login</a>
+                <a href="signup.php" class="button">Register</a>
+            </div>
+        </ul>
+    </navbar>
+
+
+    <script src="js/main.js"></script>
+</body>
+</html>
